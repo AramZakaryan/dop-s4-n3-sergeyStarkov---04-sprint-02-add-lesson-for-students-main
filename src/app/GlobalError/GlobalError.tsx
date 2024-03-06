3090ec0f89e9db9630bde23e9bed2035203a47ca
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-03-06 14:13:11 +0400

    Result of Lesson 4.2

--- src/app/GlobalError/GlobalError.tsx
+++ src/app/GlobalError/GlobalError.tsx
@@ -2,15 +2,21 @@ import { useEffect } from 'react'
 import { toast, ToastContainer } from 'react-toastify'
 import { useAppSelector } from '../store.ts'
 import { selectAppError } from '../app-selectors.ts'
+import { useDispatch } from 'react-redux'
+import { setError } from '../../common/utils/set-error.ts'
+import { setAppErrorAC } from '../app-reducer.ts'
 
 export const GlobalError = () => {
   const errorMessage = useAppSelector(selectAppError)
+  const dispatch = useDispatch()
 
   useEffect(() => {
     if (errorMessage) {
       toast.error(errorMessage)
+      dispatch(setAppErrorAC(null))
     }
   }, [errorMessage])
 
   return <ToastContainer theme="dark" autoClose={3000} />
+
 }
